531666318508eb5949d401f2bcb4e249da48648b
SameerGhela <u21445142@tuks.co.za>
2022-10-30 16:13:37 +0200

    Add project files.

---
+++ Homework6New/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Homework6New.Models;
+using System.Web.UI.DataVisualization.Charting;
+using System.IO;
+using System.Text;
+using System.Drawing;
+using Newtonsoft.Json;
+
+namespace Homework6New.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private BikeStoresEntities db = new BikeStoresEntities();
+
+      
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Orders()
+        {
+            return View();
+        }
+        public ActionResult Report()
+        {
+            ViewBag.DataPoints = JsonConvert.SerializeObject(DataService.GetRandomDataForCategoryAxis(10), _jsonSetting);
+            return View();
+        }
+        JsonSerializerSettings _jsonSetting = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore };
+    }
+}
